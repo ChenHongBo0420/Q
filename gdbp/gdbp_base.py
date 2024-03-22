439dd7742b61752a8f78d0d31b95f132ba8b7ca0
JushuaC <1047280552@qq.com>
2024-03-23 07:38:29 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -272,7 +272,7 @@ def loss_fn(module: layer.Layer,
     z_transformed_real1 = jnp.abs(z_transformed1.val) 
     z_transformed1_real1 = jax.lax.stop_gradient(z_transformed_real1)
     contrastive_loss = negative_cosine_similarity(z_original_real, z_transformed_real1)
-    aligned_y = y[z_original.t.start * 2:z_original.t.stop]
+    aligned_y = y[0:500]
     # mse_loss = jnp.mean(jnp.abs(z_original.val - x) ** 2)     
     mse_loss = jnp.mean(jnp.abs(aligned_y - z_original.val) ** 2)   
     total_loss = mse_loss + contrastive_loss
