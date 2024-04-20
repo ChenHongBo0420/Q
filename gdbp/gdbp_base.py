b142d466c994058a1c4a1358b5ef4dd2304a6c03
JushuaC <1047280552@qq.com>
2024-04-21 02:51:57 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -292,8 +292,9 @@ def loss_fn(module: layer.Layer,
     z_transformed1, _ = module.apply(
         {'params': params, 'aux_inputs': aux, 'const': const, **state}, core.Signal(y_transformed1))       
               
-    # aligned_x = x[z_original.t.start:z_original.t.stop]
-    # mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
+    aligned_x = x[z_original.t.start:z_original.t.stop]
+    mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
+    mmse_loss = jnp.mean(jnp.abs(dd.val - aligned_x) ** 2)
     feature_1 = z_original.val[:, 0]
     feature_2 = z_original.val[:, 1]
     f1 = y[:, 0]
@@ -304,12 +305,14 @@ def loss_fn(module: layer.Layer,
     f2 = jnp.abs(f2)
     z_original_real = jnp.abs(z_original.val)
     ddd = jnp.abs(dd.val) 
-    z_original = z_original + ddd
-    z_transformed_real1 = jnp.abs(z_transformed1.val) 
-    z_transformed1_real1 = jax.lax.stop_gradient(z_transformed_real1)
-    mse_loss = jnp.mean((feature_1 - feature_2) ** 2)
-    contrastive_loss = negative_cosine_similarity(z_original_real, z_transformed1_real1)  
-    total_loss = mse_loss + contrastive_loss
+    total_loss = mse_loss + mmse_loss         
+    # z_original = z_original + ddd
+    # z_transformed_real1 = jnp.abs(z_transformed1.val) 
+    # z_transformed1_real1 = jax.lax.stop_gradient(z_transformed_real1)
+    # mse_loss = jnp.mean((feature_1 - feature_2) ** 2)
+    # contrastive_loss = negative_cosine_similarity(z_original_real, z_transformed1_real1)  
+    # total_loss = mse_loss + contrastive_loss
+              
     return total_loss, updated_state
 
 @partial(jit, backend='cpu', static_argnums=(0, 1))
