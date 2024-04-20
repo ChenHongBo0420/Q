29371ef1d36ae0e0d4d86416e81849b369ca4501
JushuaC <1047280552@qq.com>
2024-04-20 15:14:01 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -290,7 +290,7 @@ def loss_fn(module: layer.Layer,
         {'params': params, 'aux_inputs': aux, 'const': const, **state}, core.Signal(y))
     z_transformed1, _ = module.apply(
         {'params': params, 'aux_inputs': aux, 'const': const, **state}, core.Signal(y_transformed1))       
- 
+    print("Dimensions of z_original:", z_original.shape)
     # aligned_x = x[z_original.t.start:z_original.t.stop]
     # mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
     z_original_real = jnp.abs(z_original.val)   
