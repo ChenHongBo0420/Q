d8ff62837e5f2a3946275abd4338575230ee974f
JushuaC <1047280552@qq.com>
2024-04-23 22:39:09 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -313,7 +313,7 @@ def loss_fn(module: layer.Layer,
     z_transformed1, _ = module.apply(
         {'params': params, 'aux_inputs': aux, 'const': const, **state}, core.Signal(y_transformed1))       
 
-    # aligned_x = x[z_original.t.start:z_original.t.stop]
+    aligned_x = x[z_original.t.start:z_original.t.stop]
     # mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
          
     # feature_1 = z_original.val[:, 0]
@@ -325,10 +325,10 @@ def loss_fn(module: layer.Layer,
     power_variance_loss = jnp.var(jnp.abs(z_original.val)**2)
     # mse_loss = jnp.mean((feature_1 - feature_2) ** 2)
     contrastive_loss = negative_cosine_similarity(z_original_real, z_transformed1_real1)  
-    snr = si_snr(jnp.abs(z_original.val), jnp.abs(z_transformed1.val))
+    snr = si_snr(jnp.abs(z_original.val), jnp.abs(aligned_x))
     # total_loss = mse_loss + batch_power_loss + power_variance_loss
     total_loss = contrastive_loss + batch_power_loss + power_variance_loss + snr
-    return total_loss, updated_state
+    return snr, updated_state
 
 @partial(jit, backend='cpu', static_argnums=(0, 1))
 def update_step(module: layer.Layer,
