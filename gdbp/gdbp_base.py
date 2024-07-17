b9aefa12e00202c3ee467b2c0b1d9ccf6ab5bee7
JushuaC <1047280552@qq.com>
2024-07-17 20:12:11 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -283,7 +283,7 @@ def loss_fn(module: layer.Layer,
     # z_transformed_real1 = jax.lax.stop_gradient(z_transformed_real1)
     # contrastive_loss = negative_cosine_similarity(z_original_real, z_transformed_real1)
     # snr = si_snr(jnp.abs(z_original.val), jnp.abs(aligned_x))
-    snr = si_snr(z_transformed1_real1, z_original_real)
+    snr = si_snr(z_transformed_real1, z_original_real)
     return snr, updated_state
 
 @partial(jit, backend='cpu', static_argnums=(0, 1))
