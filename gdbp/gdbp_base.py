0ce3dc597d70e67b455c537d6f23e2d6986f3d04
JushuaC <1047280552@qq.com>
2025-02-15 01:50:13 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -498,7 +498,7 @@ def train(model: Model,
           data: gdat.Input,
           batch_size: int = 500,
           n_iter = None,
-          opt: optim.Optimizer = optim.adam(optim.piecewise_constant([500, 1000], [1e-4, 1e-5, 1e-6]))):
+          opt: optim.Optimizer = optim.adamw(optim.piecewise_constant([500, 1000], [1e-4, 1e-5, 1e-6]))):
     ''' training process (1 epoch)
 
         Args:
