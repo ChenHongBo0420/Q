a77b28b3bbb0692c809b61812a03f8808083dc9e
JushuaC <1047280552@qq.com>
2025-03-19 17:46:05 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -554,7 +554,7 @@ def loss_fn(module: layer.Layer,
     aligned_x = x[z_original.t.start:z_original.t.stop]
     
     if loss_type == 'si_snr':
-        loss = si_snr(z_original.val, aligned_x)
+        loss = si_snr(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
     elif loss_type == 'gmi_loss':
         loss = gmi_loss_16qam(z_original.val, aligned_x)
     elif loss_type == 'combined':
