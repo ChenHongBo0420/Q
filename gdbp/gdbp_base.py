a65bb2d06cb6eadd7ad0dfe624303fa797dea0ed
JushuaC <1047280552@qq.com>
2025-03-19 18:10:35 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -536,7 +536,7 @@ def loss_fn(module: layer.Layer,
             aux: Dict,
             const: Dict,
             sparams: Dict,
-            loss_type: str = 'si_snr'):
+            loss_type: str = 'gmi_loss'):
     """
     扩展后的 loss_fn 支持三种损失计算方式：
       - 'si_snr'  : SI-SNR loss（适用于复数信号，使用共轭内积）
@@ -558,7 +558,7 @@ def loss_fn(module: layer.Layer,
     elif loss_type == 'gmi_loss':
         loss = gmi_loss_16qam(z_original.val, aligned_x)
     elif loss_type == 'combined':
-        loss = si_snr(z_original.val, aligned_x) - jnp.real(gmi_loss_16qam(z_original.val, aligned_x))
+        loss = si_snr(z_original.val, aligned_x) + gmi_loss_16qam(z_original.val, aligned_x)
     else:
         raise ValueError("Unknown loss type: " + loss_type)
     
