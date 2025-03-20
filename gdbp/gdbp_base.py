61978214191926b1171c4ad345c784b62147340b
JushuaC <1047280552@qq.com>
2025-03-20 17:19:17 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -532,7 +532,7 @@ def gmi_loss_16qam(
     target: jnp.ndarray,
     estimate: jnp.ndarray,
     constellation: jnp.ndarray = None,
-    tau: float = 0.4,
+    tau: float = 0.6,
     eps: float = 1e-8,
 ) -> jnp.ndarray:
     """
