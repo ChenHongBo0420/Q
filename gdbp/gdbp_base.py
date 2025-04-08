0e4fa11645343bc9a6a6ca72d393534252185e02
JushuaC <1047280552@qq.com>
2025-04-09 03:15:42 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -515,9 +515,10 @@ def loss_fn(module: layer.Layer,
     z_original, updated_state = module.apply(
         {'params': params, 'aux_inputs': aux, 'const': const, **state}, core.Signal(y)) 
     aligned_x = x[z_original.t.start:z_original.t.stop]
+    aligned_y = x[z_original.t.start:z_original.t.stop]
     mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
     snr1 = si_snr(jnp.abs(z_original.val), jnp.abs(aligned_x))
-    snr2 = si_snr(jnp.abs(y), jnp.abs(aligned_x))
+    snr2 = si_snr(jnp.abs(aligned_y), jnp.abs(aligned_x))
     snr = snr1 -snr2
     # snr = si_snr_flattened(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
     return snr, updated_state
