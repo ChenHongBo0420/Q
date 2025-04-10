c7c76963edae261c9617c9e6ef5b2ea9ed6fe966
JushuaC <1047280552@qq.com>
2025-04-10 16:48:07 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -121,13 +121,13 @@ def make_base_module(steps: int = 3,
                    d_init=d_init,
                    n_init=n_init),
         layer.BatchPowerNorm(mode=mode),
-        layer.MIMOFOEAf1(name='FOEAf',
+        layer.MIMOFOEAf(name='FOEAf',
                         w0=w0,
                         train=mimo_train,
                         preslicer=core.conv1d_slicer(rtaps),
                         foekwargs={}),
         layer.vmap(layer.Conv1d)(name='RConv', taps=rtaps),
-        layer.MIMOAF1(train=mimo_train),
+        layer.MIMOAF(train=mimo_train),
         name='serial_branch'  # 添加名称
     )
 
