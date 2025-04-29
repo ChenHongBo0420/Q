a5eff1e00b96adc3b88263c12501b2ce0ba35146
JushuaC <1047280552@qq.com>
2025-04-29 22:41:39 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -479,10 +479,10 @@ def loss_fn(module: layer.Layer,
     # y_transformed = apply_combined_transform(y)
     aligned_x = x[z_original.t.start:z_original.t.stop]
     mse_loss = jnp.mean(jnp.abs(z_original.val - aligned_x) ** 2)
+    snr = si_snr(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
     # snr = si_snr_flat_amp_pair(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
-    snr = si_snr_flat_amp_pair(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
-    evm_loss = evm_ring(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
-    phase_loss = phase_err(jnp.abs(z_original.val), jnp.abs(aligned_x))
+    # evm_loss = evm_ring(jnp.abs(z_original.val), jnp.abs(aligned_x)) 
+    # phase_loss = phase_err(jnp.abs(z_original.val), jnp.abs(aligned_x))
     # snr = snr + 0.001 * evm_loss
     return snr, updated_state
                    
