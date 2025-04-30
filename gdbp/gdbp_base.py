82e5408e6e39bf686c8d820adc215824bf201e57
JushuaC <1047280552@qq.com>
2025-04-30 18:04:40 +0900

    Update gdbp_base.py

--- gdbp/gdbp_base.py
+++ gdbp/gdbp_base.py
@@ -515,7 +515,7 @@ def loss_fn(module, params, state,
                               tau=0.05, gamma=2.0,
                               step=step)          # ★ 传 step
 
-    total = snr_loss + 0.05 * evm_loss
+    total = snr_loss + 0.01 * evm_loss
     return total, new_state
 
 
