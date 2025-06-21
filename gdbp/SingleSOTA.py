ab67f4756c5180bea5e3740e6047b2ebca78a23a
JushuaC <1047280552@qq.com>
2025-06-22 06:12:09 +0900

    Update SingleSOTA.py

--- gdbp/SingleSOTA.py
+++ gdbp/SingleSOTA.py
@@ -433,7 +433,7 @@ def train(model: Model,
 def test(model: Model,
          params: Dict,
          data: gdat.Input,
-         eval_range: tuple=(0, 0),
+         eval_range: tuple=(0, -20000),
          metric_fn=comm.qamqot):
     ''' testing, a simple forward pass
 
