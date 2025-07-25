aaa4fcd97749a6a58c1de77e53b3215bf2c0a5b2
JushuaC <1047280552@qq.com>
2025-07-26 01:10:37 +0900

    Update SOTANN.py

--- gdbp/SOTANN.py
+++ gdbp/SOTANN.py
@@ -50,13 +50,13 @@ def make_base_module(steps: int = 3,
                    d_init=d_init,
                    n_init=n_init),
         layer.BatchPowerNorm(mode=mode),
-        layer.MIMOFOEAf(name='FOEAf',
-                        w0=w0,
-                        train=mimo_train,
-                        preslicer=core.conv1d_slicer(rtaps),
-                        foekwargs={}),
-        layer.vmap(layer.Conv1d)(name='RConv', taps=rtaps),  # vectorize column-wise Conv1D
-        layer.MIMOAF(train=mimo_train))  # adaptive MIMO layer
+        # layer.MIMOFOEAf(name='FOEAf',
+        #                 w0=w0,
+        #                 train=mimo_train,
+        #                 preslicer=core.conv1d_slicer(rtaps),
+        #                 foekwargs={}),
+        # layer.vmap(layer.Conv1d)(name='RConv', taps=rtaps),  # vectorize column-wise Conv1D
+        # layer.MIMOAF(train=mimo_train))  # adaptive MIMO layer
         
     return base
 
