29504aa6a6d57cef2df406a6ad66f1da2d7d86c2
JushuaC <1047280552@qq.com>
2026-01-05 04:19:33 +0900

    Update MSE.py

--- gdbp/MSE.py
+++ gdbp/MSE.py
@@ -74,7 +74,8 @@ def make_base_module(steps: int = 3,
                         preslicer=core.conv1d_slicer(rtaps),
                         foekwargs={}),
         layer.vmap(layer.Conv1d)(name='RConv', taps=rtaps),  # vectorize column-wise Conv1D
-        # layer.MIMOAF(train=mimo_train))  # adaptive MIMO layer
+        # layer.MIMOAF(train=mimo_train)
+        )  
         
     return base
 
